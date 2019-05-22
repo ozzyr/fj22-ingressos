679c357af2e1a8997b5f3a0352113ee71a39eb7b
MagnoOzzyr <maxozzyr@gmail.com>
2019-05-21 21:21:21 -0300

    package added

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,3 +1,5 @@
+package br.com.caelum.ingresso.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,48 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+public class Sessao {
+    private Integer id;
+    private LocalTime horario;
+    private Sala sala;
+    private Filme filme; 
+
+    public Sessao(LocalTime horario, Filme filme, Sala sala){
+        this.horario = horario;
+        this.filme = filme;
+        this.sala = sala;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+    public Filme getFilme() {
+        return this.filme;
+    }
+
+    public Sala getSala() {
+        return this.sala;
+    }
+     
+    public LocalTime getHorario() {
+        return horario;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+   
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public void setHorario(LocalTime horario) {
+        this.horario = horario;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
